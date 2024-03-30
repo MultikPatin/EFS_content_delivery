c99deffb49dde25e68716976f7264ec70693a755
Dmitry Patin <multik.patin@yandex.ru>
2024-03-30 17:25:43 +0300

    add abstract class

---
+++ src/api/db/cache/abstract.py
@@ -0,0 +1,90 @@
+from abc import ABC, abstractmethod
+from typing import TypeVar
+
+from pydantic import BaseModel
+
+ModelType = TypeVar("ModelType", bound=BaseModel)
+
+
+class AbstractCache(ABC):
+    """
+    Abstract base class for caching.
+
+    Provides an interface for caching objects of type ModelType.
+    """
+
+    @abstractmethod
+    async def set_one_model(
+        self,
+        key: str,
+        value: ModelType,
+        cache_expire: int,
+    ) -> None:
+        """
+        Set a single model in the cache.
+
+        Args:
+            key (str): The key to use for caching the model.
+            value (ModelType): The model to cache.
+            cache_expire (int): The number of seconds until the model expires.
+        """
+        pass
+
+    @abstractmethod
+    async def get_one_model(self, key: str, model) -> ModelType | None:
+        """
+        Get a single model from the cache.
+
+        Args:
+            key (str): The key used for caching the model.
+            model (ModelType): The model class to cast the cached value to.
+
+        Returns:
+            The cached model, or None if the model is not in the cache.
+        """
+        pass
+
+    @abstractmethod
+    async def set_list_model(
+        self,
+        key: str,
+        values: list[ModelType],
+        cache_expire: int,
+    ) -> None:
+        """
+        Set a list of models in the cache.
+
+        Args:
+            key (str): The key to use for caching the list of models.
+            values (list[ModelType]): The list of models to cache.
+            cache_expire (int): The number of seconds until the list of models expires.
+        """
+        pass
+
+    @abstractmethod
+    async def get_list_model(self, key: str, model) -> list[ModelType] | None:
+        """
+        Get a list of models from the cache.
+
+        Args:
+            key (str): The key used for caching the list of models.
+            model (ModelType): The model class to cast the cached values to.
+
+        Returns:
+            The cached list of models, or None if the list of models is not in the cache.
+        """
+        pass
+
+    @abstractmethod
+    def build_key(self, key_prefix: str, *args) -> str:
+        """
+        Build a cache key.
+
+        Args:
+            key_prefix (str): The prefix to use for the cache key.
+            *args: Additional arguments to include in the cache key.
+
+        Returns:
+            The built cache key.
+        """
+        pass
