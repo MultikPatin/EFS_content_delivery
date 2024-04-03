1285fc9a663b6a37ec4b6e3902f382be65a97d03
Dmitry Patin <multik.patin@yandex.ru>
2024-04-03 21:39:22 +0300

    fix docker-compose.test.yml

--- tests/functional/utils/wait_for_es.py
+++ tests/functional/utils/wait_for_es.py
@@ -1,13 +1,27 @@
+import asyncio
 import time
 
 from elasticsearch import AsyncElasticsearch
 
 from tests.functional.settings import settings
 
-if __name__ == "__main__":
-    client = AsyncElasticsearch(hosts=settings.get_es_host, verify_certs=False)
+
+async def ping_check(es_client: AsyncElasticsearch) -> None:
     while True:
         print("Waiting for Elasticsearch")
-        if client.ping():
+        if await es_client.ping():
+            print("Elasticsearch Ready")
+            await es_client.close()
             break
         time.sleep(1)
+
+
+if __name__ == "__main__":
+    client = AsyncElasticsearch(hosts=settings.get_es_host, verify_certs=False)
+
+    ioloop = asyncio.get_event_loop()
+    tasks = [
+        ioloop.create_task(ping_check(client)),
+    ]
+    ioloop.run_until_complete(asyncio.wait(tasks))
+    ioloop.close()
