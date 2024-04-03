64793d4f9d07c1284c093ed3b73e2b7f50276413
Dmitry Patin <multik.patin@yandex.ru>
2024-04-03 21:02:46 +0300

    add docker-compose.test.yml

---
+++ tests/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python3 /tests/functional/utils/wait_for_es.py
+
+python3 /tests/functional/utils/wait_for_redis.py
+
+pytest
--- tests/functional/utils/wait_for_redis.py
+++ tests/functional/utils/wait_for_redis.py
@@ -2,11 +2,12 @@ import time
 
 from redis.asyncio import Redis
 
-from tests.functional.settings import test_settings
+from tests.functional.settings import settings
 
 if __name__ == "__main__":
-    client = Redis(**test_settings.get_redis_host)
+    client = Redis(**settings.get_redis_host)
     while True:
+        print("Waiting for Redis")
         if client.ping():
             break
         time.sleep(1)
